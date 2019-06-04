911245ae5d68e07a51d9b6b287c79f1f3defe5bd
Raul Sposito <raulsposito@gmail.com>
2019-06-04 00:32:17 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,4 @@
 # Define the variable board below.
-row = [" "," "," "]
-
-# Position 1: Left 
-row[0]
-
-# Position 2: Middle
-row[1]
-
-# Position 3: Right
-row[2]
 
 def 
   board [" "," "," "," "," "," "]
